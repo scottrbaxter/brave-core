d159be5292e7878e732c887a57129cf3eeaf9d6a
Aleksey Seren <aseren@brave.com>
2021-09-16 21:36:19 +0700

    Add braveRequestAdsEnabled android implementation.
    fix https://github.com/brave/brave-browser/issues/17591

--- browser/brave_ads/brave_ads_host_android.cc
+++ browser/brave_ads/brave_ads_host_android.cc
@@ -7,33 +7,104 @@
 
 #include <utility>
 
+#include "base/check.h"
 #include "brave/browser/brave_ads/ads_service_factory.h"
+#include "brave/browser/brave_ads/android/jni_headers/BraveAdsHostAndroid_jni.h"
+#include "brave/browser/brave_rewards/rewards_service_factory.h"
 #include "brave/components/brave_ads/browser/ads_service.h"
+#include "brave/components/brave_rewards/browser/rewards_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/web_contents.h"
 
+using base::android::AttachCurrentThread;
+using base::android::JavaParamRef;
+
 namespace brave_ads {
 
-BraveAdsHostAndroid::BraveAdsHostAndroid(content::WebContents* web_contents)
-    : web_contents_(web_contents) {
-  DCHECK(web_contents_);
+BraveAdsHostAndroid::BraveAdsHostAndroid(content::WebContents* web_contents) {
+  DCHECK(web_contents);
+  profile_ = Profile::FromBrowserContext(web_contents->GetBrowserContext());
+  DCHECK(profile_);
+
+  java_object_.Reset(Java_BraveAdsHostAndroid_create(
+      AttachCurrentThread(), reinterpret_cast<intptr_t>(this)));
 }
 
-BraveAdsHostAndroid::~BraveAdsHostAndroid() = default;
+BraveAdsHostAndroid::~BraveAdsHostAndroid() {
+  Java_BraveAdsHostAndroid_closeBraveTalkOptInPopup(AttachCurrentThread(),
+                                                    java_object_);
+  Java_BraveAdsHostAndroid_destroy(AttachCurrentThread(), java_object_);
+}
 
 void BraveAdsHostAndroid::RequestAdsEnabled(
     RequestAdsEnabledCallback callback) {
-  Profile* profile =
-      Profile::FromBrowserContext(web_contents_->GetBrowserContext());
-  DCHECK(profile);
-
-  const AdsService* ads_service = AdsServiceFactory::GetForProfile(profile);
-  if (!ads_service) {
+  if (callback_) {
     std::move(callback).Run(false);
     return;
   }
 
-  std::move(callback).Run(ads_service->IsEnabled());
+  const AdsService* ads_service = AdsServiceFactory::GetForProfile(profile_);
+  brave_rewards::RewardsService* rewards_service =
+      brave_rewards::RewardsServiceFactory::GetForProfile(profile_);
+  if (!rewards_service || !ads_service || !ads_service->IsSupportedLocale()) {
+    std::move(callback).Run(false);
+    return;
+  }
+
+  if (ads_service->IsEnabled()) {
+    std::move(callback).Run(true);
+    return;
+  }
+
+  callback_ = std::move(callback);
+  rewards_service_observation_.Observe(rewards_service);
+
+  Java_BraveAdsHostAndroid_openBraveTalkOptInPopup(AttachCurrentThread(),
+                                                   java_object_);
+}
+
+void BraveAdsHostAndroid::OnAdsEnabled(
+    brave_rewards::RewardsService* rewards_service,
+    bool ads_enabled) {
+  DCHECK(rewards_service);
+
+  RunCallbackAndReset(ads_enabled);
+}
+
+void BraveAdsHostAndroid::NotifyAdsEnabled(JNIEnv* env,
+                                           const JavaParamRef<jobject>& obj) {
+  DCHECK(callback_);
+
+  brave_rewards::RewardsService* rewards_service =
+      brave_rewards::RewardsServiceFactory::GetForProfile(profile_);
+  if (!rewards_service) {
+    RunCallbackAndReset(false);
+    return;
+  }
+
+  rewards_service->EnableRewards();
+  enable_ads_pressed_ = true;
+}
+
+void BraveAdsHostAndroid::NotifyPopupClosed(JNIEnv* env,
+                                            const JavaParamRef<jobject>& obj) {
+  if (!callback_) {
+    return;
+  }
+
+  // If ads enable was pressed then do nothing and wait for ads enabled event.
+  if (!enable_ads_pressed_) {
+    RunCallbackAndReset(false);
+  }
+}
+
+void BraveAdsHostAndroid::RunCallbackAndReset(bool ads_enabled) {
+  DCHECK(callback_);
+
+  enable_ads_pressed_ = false;
+  rewards_service_observation_.Reset();
+
+  std::move(callback_).Run(ads_enabled);
 }
 
 }  // namespace brave_ads
